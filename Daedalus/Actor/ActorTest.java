f389dfd9c1bc591b5d778f9bd8da4a7bd7918ce7
Michael <MichaelWidler@yahoo.com>
2026-09-10 17:22:27 -0700

    Test damaging actor with armor and shield.

--- Daedalus/Actor/ActorTest.java
+++ Daedalus/Actor/ActorTest.java
@@ -1,5 +1,7 @@
 package Daedalus.Actor;
 
+import Daedalus.Item.*;
+import Daedalus.Combat.*;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -72,5 +74,33 @@ public class ActorTest implements ActorConstants
       a.getBaseStats().setInteractSpeed(ActionSpeed.INSTANTANEOUS);
       a.getBaseStats().add(modBlock);
       Assert.assertEquals("INSTANTANEOUS Base speed + SLOW is INSTANTANEOUS", ActionSpeed.INSTANTANEOUS, a.getInteractSpeed());
+   }
+   
+   
+   @Test public void testTakingDamage() 
+   {
+      a.getBaseStats().setMaxHealth(10);
+      a.fullHeal();
+      Armor armor = new Armor("Test Armor");
+      armor.setDamageProtection(CombatConstants.DamageType.FIRE, 5);
+      Shield shield = new Shield("Test Shield");
+      shield.setMaxDamageCapacity(5);
+      shield.fullyCharge();
+      a.setArmor(armor);
+      a.setShield(shield);
+      Damage damage = new Damage();
+      damage.setValue(CombatConstants.DamageType.FIRE, 2);
+      damage.setValue(CombatConstants.DamageType.COLD, 2);
+      
+      Assert.assertEquals("Initially health full", 10, a.getCurHealth());
+      Assert.assertEquals("Initially shield full", 5, a.getCurShield());
+      
+      a.applyDamage(damage);
+      Assert.assertEquals("Shield absorbs all initial damage", 10, a.getCurHealth());
+      Assert.assertEquals("Shield absorbs all initial damage", 1, a.getCurShield());
+      
+      a.applyDamage(damage);
+      Assert.assertEquals("Armor fully applies to overflow damage", 9, a.getCurHealth());
+      Assert.assertEquals("Armor fully applies to overflow damage", 0, a.getCurShield());
    }
 }
